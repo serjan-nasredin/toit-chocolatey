db55227b06d18f18c14d059d8bf976c95bf202f5
snxx-lppxx <snmgreenx@gmail.com>
2022-01-20 02:18:41 +0500

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,12 +1,20 @@
-$url = 'https://github.com/toitlang/toit/releases/download/v0.11.1/toit-windows.tar.gz'
+# Copyright: GNU GPL v3.0
+# Date: 26.10.2021 - 20.01.2022
+# Author: Toitware ApS <xxx@toit.io>
+
+$ErrorActionPreferense = 'Stop';
+
+$toolDir = "$(Split-Path -parent)"
+$url = 'https://github.com/toitlang/toit/releases/download/v0.15.0/toit-windows.tar.gz'
 
 $packageArgs = @{ 
-    packageName = 'Toit'
-    unzipLocation = $toolDir
-    fileType = 'EXE'
-    url = $url
+	packageName = 'Toit'
+	unzipLocation = $toolDir
+	fileType = 'EXE'
+	url = $url
+	fileFullPath = Join-Path $toolsDir 'toit.exe'
 
-    softwareName = 'Toit'
+	softwareName = 'Toit'
 }
 
 Get-ChocolateyWebFile $packageName -FileFullPath "\toit.exe" $url
